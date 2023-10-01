9807955f0065bf6fa7bb2d8249a9e2f53105a582
tsn <turskov.sn@yandex.ru>
2023-10-01 18:40:41 +0300

    The first create

--- src/test/java/ru/netology/selenide/DeliveryCardTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardTest.java
@@ -1,29 +1,24 @@
 package ru.netology.selenide;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.Condition;
 
-import static com.codeborne.selenide.Selenide.$$;
-import static java.nio.channels.Selector.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DeliveryCardTest {
-
-    private WebDriver driver;
-
-    //@BeforeAll
-    // static void setUpAll() {
-        // WebDriverManager.chromedriver().setup():
-    // }
-
-    @Test
-    void shouldRegisteredByAccountNumberModification() {
-        open("");
-        $$(".tab-item").last().click();
-        $()
+    @org.junit.jupiter.api.Test
+    public void fillFormFields() {
+        open("http://localhost:9999/");
+        $("[data-test-id=\"city\"] input").setValue("Нижний Новгород");
+        $("[data-test-id=\"date\"] input").setValue("04.10.2023");
+        $("[data-test-id=\"name\"] input").setValue("Турсков Сергей");
+        $("[data-test-id=\"phone\"] input").setValue("+79200000000");
+        $("[data-test-id=\"agreement\"]").click();
+        $("button.button").click();
+        $("[data-test-id=\"notification\"]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на 04.10.2023"));
     }
 }
