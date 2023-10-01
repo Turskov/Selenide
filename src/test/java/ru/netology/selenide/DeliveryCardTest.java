e1141f4179e9ba2a7536c7a6f118b04e50e35cae
tsn <turskov.sn@yandex.ru>
2023-10-01 21:33:52 +0300

    New tests created

--- src/test/java/ru/netology/selenide/DeliveryCardTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardTest.java
@@ -15,11 +15,10 @@ public class DeliveryCardTest {
         return LocalDate.now().plusDays(choiceDays).format(DateTimeFormatter.ofPattern(pattern));
     }
     @org.junit.jupiter.api.Test
-    void shouldTestIfSuccess() {
+    void shouldTestIfFormCorrectly() {
         open("http://localhost:9999");
-        
-        $("[data-test-id=city] input").setValue("Ни");
 
+        $("[data-test-id=city] input").setValue("Ни");
         $$(".menu-item").find(Condition.exactText("Нижний Новгород")).click();
 
         String futureDate = planingDate(4, "dd.MM.yyyy");
@@ -31,9 +30,86 @@ public class DeliveryCardTest {
         $("[data-test-id=agreement]").click();
         $("button.button").click();
 
-        // Проверяем уведомление
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + futureDate));
+    }
+    @org.junit.jupiter.api.Test
+    void shouldTestIfFormWithNameOnEnglish() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue("Ни");
+        $$(".menu-item").find(Condition.exactText("Нижний Новгород")).click();
+
+        String futureDate = planingDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(futureDate);
+
+        $("[data-test-id=name] input").setValue("Turskov Sergey");
+        $("[data-test-id=phone] input").setValue("+79200000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+
+        $("[data-test-id=name] .input__sub")
+                .shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @org.junit.jupiter.api.Test
+    void shouldTestIfFormIfCityIncorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Выкса");
+
+        String futureDate = planingDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(futureDate);
+
+        $("[data-test-id=name] input").setValue("Турсков Сергей");
+        $("[data-test-id=phone] input").setValue("+79200000000");
+
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+
+        $("[data-test-id=city] .input__sub")
+                .shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+    @org.junit.jupiter.api.Test
+    void shouldTestIfFormIfDAteIncorrect() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue("Ни");
+        $$(".menu-item").find(Condition.exactText("Нижний Новгород")).click();
+
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue("04.10.2022");
+
+        $("[data-test-id=name] input").setValue("Турсков Сергей");
+        $("[data-test-id=phone] input").setValue("+79200000000");
+
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+
+        $("[data-test-id=date] .input__sub")
+                .shouldHave(Condition.exactText("Заказ на выбранную дату невозможен"));
+    }
+    @org.junit.jupiter.api.Test
+    void shouldTestIfFormIfPhoneIncorrect() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue("Ни");
+        $$(".menu-item").find(Condition.exactText("Нижний Новгород")).click();
+
+        String futureDate = planingDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(futureDate);
+
+        $("[data-test-id=name] input").setValue("Турсков Сергей");
+        $("[data-test-id=phone] input").setValue("+792000000000");
+
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+
+        $("[data-test-id=phone] .input__sub")
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, " +
+                        "например, +79012345678."));
     }
 }
